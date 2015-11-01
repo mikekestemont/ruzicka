b9c333eabdcd7ff210eda847364145cc52e10de0
Mike Kestemont <mike.kestemont@gmail.com>
2015-11-01 19:19:10 +0100

    better viz

--- code/latin_testviz.py
+++ code/latin_testviz.py
@@ -9,42 +9,49 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 from distance_metrics import pairwise_minmax
 from scipy.spatial.distance import squareform, pdist
-from visualization import tsne, clustermap
+from visualization import tsne, clustermap, tree
 
 
 font = {'family' : 'arial', 'size' : 6}
 sb.plt.rc('font', **font)
 
 import numpy as np
-from mpl_toolkits.axes_grid.anchored_artists import AnchoredText
-
 from utilities import load_pan_dataset
 from vectorization import Vectorizer
 from sklearn.preprocessing import LabelEncoder
 
-corpus_dir = '../data/latin/test'
 ngram_type = 'word'
 ngram_size = 1
 mfi = 10000
-vector_space = 'tf_std'
+vector_space = 'tf'
 
-print('>>> corpus:', corpus_dir)
-# preprocess:
-test_data, _ = load_pan_dataset(corpus_dir)
-labels, documents = zip(*test_data)
+dev_data, _ = load_pan_dataset('../data/latin/dev/')
+dev_labels, dev_documents = zip(*dev_data)
 
-# vectorize:
+# fit vectorizer on dev data:
 vectorizer = Vectorizer(mfi=mfi,
                         vector_space=vector_space,
                         ngram_type=ngram_type,
                         ngram_size=ngram_size)
-X = vectorizer.fit_transform(documents).toarray()
-dm = squareform(pdist(X, pairwise_minmax))
-# scale distance matrix:
-max_ = X.max()
-min_ = X.min()
-dm = (dm-min_) / (max_ - min_)
+vectorizer.fit(dev_documents)
 
-clustermap(dm, labels)
+test_data, _ = load_pan_dataset('../data/latin/test/')
+test_labels, test_documents = zip(*test_data)
+vectorizer.fit_transform(test_documents)
+
+X = vectorizer.transform(test_documents).toarray()
+dm = squareform(pdist(X, pairwise_minmax))
+tree(dm, test_labels)
+
+# scale distance matrix:
+nonzeroes = dm[dm.nonzero()]
+max_ = nonzeroes.max()
+min_ = nonzeroes.min()
+dm = (dm-min_) / (max_ - min_)
+np.fill_diagonal(dm, 0.0)
+
+clustermap(dm, test_labels)
+
+
 
 
